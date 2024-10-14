554422832d1187f746e7ae78fdd40ca30c9f82db
Dmitry_Perexozhev <perexozhev99@gmail.com>
2024-10-15 01:06:26 +0300

    create start template

--- Makefile
+++ Makefile
@@ -4,3 +4,6 @@ install:
 
 start:
 	python -m gunicorn task_manager.asgi:application -k uvicorn.workers.UvicornWorker
+
+run:
+	python manage.py runserver
--- task_manager/settings.py
+++ task_manager/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'task_manager',
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
