dd61af377cd901168c316053fd3a85f1b1312800
Dmitry_Perexozhev <perexozhev99@gmail.com>
2024-11-19 09:48:55 +0300

    ALLOWED_HOSTS is webserver

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 # ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(', ')
-ALLOWED_HOSTS = ['webserver', '127.0.0.1']
+ALLOWED_HOSTS = ['webserver', ]
 
 
 # Application definition
