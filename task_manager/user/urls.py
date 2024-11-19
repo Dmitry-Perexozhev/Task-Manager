00c747b7f3553ce391e5e4b0f0af11192cb835f3
Dmitry_Perexozhev <perexozhev99@gmail.com>
2024-11-19 22:17:59 +0300

    edit url user update and user delete

--- task_manager/user/urls.py
+++ task_manager/user/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.ListUsers.as_view(), name='users_list'),
     path('create/', views.AddUser.as_view(), name='add_user'),
     path('logout/', views.LogoutUser.as_view(), name='logout_user'),
-    path('update/<int:pk>/', views.UpdateUser.as_view(), name='update_user'),
-    path('delete/<int:pk>/', views.DeleteUser.as_view(), name='delete_user'),
+    path('<int:pk>/update/', views.UpdateUser.as_view(), name='update_user'),
+    path('<int:pk>/delete/', views.DeleteUser.as_view(), name='delete_user'),
 ]
