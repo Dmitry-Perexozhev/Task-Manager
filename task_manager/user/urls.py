d9ead4ee678acdc91c8e21b4c5e10b8e3094a988
Dmitry_Perexozhev <perexozhev99@gmail.com>
2024-11-19 22:10:40 +0300

    rename url edit user to update user

--- task_manager/user/urls.py
+++ task_manager/user/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.ListUsers.as_view(), name='users_list'),
     path('create/', views.AddUser.as_view(), name='add_user'),
     path('logout/', views.LogoutUser.as_view(), name='logout_user'),
-    path('edit/<int:pk>/', views.UpdateUser.as_view(), name='edit_user'),
+    path('update/<int:pk>/', views.UpdateUser.as_view(), name='update_user'),
     path('delete/<int:pk>/', views.DeleteUser.as_view(), name='delete_user'),
 ]
