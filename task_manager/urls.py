d02d56849b9e8f10edc294f4428574782402c9c0
Dmitry_Perexozhev <perexozhev99@gmail.com>
2024-11-19 21:59:30 +0300

    edit url for login

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from task_manager import views
+from task_manager.views import Home
+from task_manager.user.views import LoginUser
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.Home.as_view(), name='home'),
+    path('', Home.as_view(), name='home'),
     path('users/', include('task_manager.user.urls')),
+    path('login/', LoginUser.as_view(), name='login_user'),
     path('statuses/', include('task_manager.statuses.urls')),
     path('tasks/', include('task_manager.tasks.urls')),
     path('labels/', include('task_manager.labels.urls'))
--- task_manager/user/urls.py
+++ task_manager/user/urls.py
@@ -5,7 +5,6 @@ from task_manager.user import views
 urlpatterns = [
     path('', views.ListUsers.as_view(), name='users_list'),
     path('create/', views.AddUser.as_view(), name='add_user'),
-    path('login/', views.LoginUser.as_view(), name='login_user'),
     path('logout/', views.LogoutUser.as_view(), name='logout_user'),
     path('edit/<int:pk>/', views.UpdateUser.as_view(), name='edit_user'),
     path('delete/<int:pk>/', views.DeleteUser.as_view(), name='delete_user'),
